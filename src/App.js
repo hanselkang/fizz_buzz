d21c80a6733e2055bc676dbf7cdf34a3dd55e565
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-05-11 13:15:02 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(0)
+  const [answer, setAnswer] = useState('')
+  const [guess, setGuess] = useState('')
+
+  useEffect(() => {
+    fizzNumber(number)
+  }, [number])
+
+  const handleInput = (event) => {
+    event.preventDefault()
+    setGuess(event.target.value)
+  }
+
+  const numberInc = () => {
+    setNumber(number + 1)
+  }
+
+  const fizzNumber = (inputNumber) => {
+    if (inputNumber === 0) {
+      setAnswer(inputNumber);
+    }
+    else if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+      setAnswer("fizzbuzz")
+    } else if (inputNumber % 3 === 0) {
+      setAnswer("fizz")
+    } else if (inputNumber % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(inputNumber);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button value={number} onClick={numberInc}>Next Number</button>
+      <h1>Number is now {number}</h1>
+      <h2>FizzBuzz Number is {answer}</h2>
+      <input type="text" onChange={handleInput}></input>
+      <h3>
+        {guess == answer ? <h1>Right</h1> : <h1>Wrong</h1>}
+      </h3>
     </div>
   );
 }
